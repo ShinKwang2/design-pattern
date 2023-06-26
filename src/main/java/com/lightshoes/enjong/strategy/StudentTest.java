6d008b32b811f5ed2c9f37f5f1af1a2067253756
kwang <shinkwang@yonsei.ac.kr>
2023-06-26 21:26:12 +0900

    style: Change name of class

--- src/main/java/com/lightshoes/enjong/strategy/StudentTest.java
+++ src/main/java/com/lightshoes/enjong/strategy/StudentTest.java
@@ -1,6 +1,6 @@
 package com.lightshoes.enjong.strategy;
 
-public class StudenTest {
+public class StudentTest {
 
     public static void main(String[] args) {
         Student studentLee = new Student(1001, "Lee");
